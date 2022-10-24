40a2a070823cc8ee49a612c32490e12ba973446d
SpringLoach <1007435098@qq.com>
2022-10-25 00:11:55 +0800

    renew

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,5 @@
 const devBaseURL = "/api";
-const proBaseURL = "/api";
+const proBaseURL = "http://120.79.30.238:8000";
 export const BASE_URL =
   process.env.NODE_ENV === "development" ? devBaseURL : proBaseURL;
 
