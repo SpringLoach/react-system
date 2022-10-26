22988ff8ace8c5504f898a17bf46c1e6bdd02c8f
Master1998 <1007435098@qq.com>
2022-10-26 11:56:56 +0800

    补充页脚配置模块

--- src/pages/main/account/footer-config/index.js
+++ src/pages/main/account/footer-config/index.js
@@ -1,9 +1,34 @@
-import React, { memo } from 'react'
+import React, { memo, useState } from 'react'
 
-import { Card, Switch } from "antd"
-import { FooterConfigWrapper, PageWrapper } from "./style"
+import { Card, Switch, Radio, Button, Input, message } from "antd"
+import { FooterConfigWrapper, PageWrapper, ColorItem, AddBtn } from "./style"
 
 export default memo(() => {
+  const [tipList, setTipList] = useState(['赢得别人注意的最好方法', 'c', 'd', 'a']);
+
+  const addTip = () => {
+    if (tipList.length >= 8) {
+      message.info('可使用的提示语最多不超过8个');
+      return
+    }
+    const target = [...tipList]
+    target.push('')
+    setTipList(target)
+  }
+
+  const deleteTip = (index) => {
+    if (tipList.length === 1) {
+      message.info('至少需要添加一个提示语');
+      return
+    }
+    const target = [...tipList]
+    target.splice(index, 1)
+    setTipList(target)
+  }
+
+  const save = () => {
+    message.info('todo');
+  }
   return (
     <div>
       <FooterConfigWrapper>
@@ -12,10 +37,36 @@ export default memo(() => {
       </FooterConfigWrapper>
       <PageWrapper>
         <Card>
-          <div>
-            <span>展示页脚&nbsp;&nbsp;</span>
-            <Switch defaultChecked />
+          <div className='control-row'>
+            <span className='label'>展示页脚：</span>
+            <Switch defaultChecked checkedChildren="开启" unCheckedChildren="关闭" />
           </div>
+          <div className='control-row'>
+            <span className='label'>文字颜色：</span>
+            <Radio.Group>
+              <Radio value={1}><ColorItem color="rgba(0,0,0,0.45);" /></Radio>
+              <Radio value={2}><ColorItem color="#faad14" /></Radio>
+              <Radio value={3}><ColorItem color="#52c41a" /></Radio>
+              <Radio value={4}><ColorItem color="#e6f7ff" /></Radio>
+              <Radio value={5}><ColorItem color="#fcbdbf" /></Radio>
+            </Radio.Group>
+          </div>
+          <div className='control-row'>
+            <span className='label'>提示语：</span>
+            <AddBtn onClick={addTip}>新增提示语</AddBtn>
+            <span style={{ color: '#d3d3d3' }}>(最多添加8项)</span>
+          </div>
+          {
+            tipList.map((item, index) => {
+              return (
+                <div className='input-row' key={index}>
+                  <Input placeholder="请输入提示语" value={item} />
+                  <Button type="link" danger onClick={() => { deleteTip(index) }}>删除</Button>
+                </div>
+              )
+            })
+          }
+          <Button type="primary" className='sure-btn' onClick={save}>保存</Button>
         </Card>
       </PageWrapper>
     </div>
--- src/pages/main/account/footer-config/style.js
+++ src/pages/main/account/footer-config/style.js
@@ -15,4 +15,39 @@ export const PageWrapper = styled.div`
   .card {
     box-shadow: 0px 0px 12px rgba(0, 0, 0, .06);
   }
+  .control-row {
+    margin-bottom: 24px;
+    display: flex;
+    align-items: center;
+  }
+  .label {
+    display: inline-block;
+    width: 80px;
+  }
+  .input-row {
+    margin: 0 0 30px 80px;
+    input {
+      width: 500px;
+    }
+  }
+  .sure-btn {
+    margin-left: 80px;
+  }
 `;
+
+export const ColorItem = styled.span`
+  display: inline-block;
+  width: 60px;
+  height: 16px;
+  transform: translateY(3px);
+  background: ${props => props.color};
+`
+
+export const AddBtn = styled.span`
+  cursor: pointer;
+  color: #1890ff;
+  margin-right: 6px;
+  &:hover {
+    color: #40a9ff;
+  }
+`
